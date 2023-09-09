31c0dcd96391ee050f45ab48c734755d001c49d1
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-09-09 08:52:10 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+
+	/* your code goes there */
+	int num;
+	for (num = 0; num <= 9; num++)
+	{
+		putchar("%d", num)
+	}
+	putchar('\n')
+	return (0);
+}
