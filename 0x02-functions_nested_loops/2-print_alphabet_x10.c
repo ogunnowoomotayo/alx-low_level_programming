8d8ee6f36ce7ad1d1bc5746fc9f9c2f67231b035
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-09-13 06:16:09 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet_x10 in lowwer case followed by new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
