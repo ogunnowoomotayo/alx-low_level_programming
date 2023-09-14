7d58fe89d0e597c36d622ab6116c6774968eb3b4
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-09-14 05:44:56 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - part of the code causing an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	printf("Infinite loop incoming :(\n");
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
