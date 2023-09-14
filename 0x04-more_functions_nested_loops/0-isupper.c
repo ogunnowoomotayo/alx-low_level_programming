4f109e31d526b7686992befdeadcc1cd6e5c7407
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-09-14 13:24:16 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checking for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 if other wise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+my 0x04-more nested function
