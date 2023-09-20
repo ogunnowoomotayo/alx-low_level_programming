5374598dc947369443711d74fb0e0e791765d1e7
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-09-20 07:14:23 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - write function that print the last digit of a number
+ * @n: data type
+ * Return: return the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int j = n % 10;
+
+	return (j);
+}
