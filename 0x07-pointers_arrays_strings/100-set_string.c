4d4422cf87202ca3cf247eb1114319ac1bf49ac6
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-09-26 17:39:30 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * set_string - function that sets the value of a pointer to a char
+ * @s: pointer, pointing to another variable
+ * @to: parameter 2
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	
+}
