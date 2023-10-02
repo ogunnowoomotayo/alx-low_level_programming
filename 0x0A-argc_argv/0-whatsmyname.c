06139c854f2a04f2726c34eeb1b7b494a2ae16ef
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-10-02 14:54:23 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that print is name
+ * @argc: 1 parameter
+ * @argv: second parameter
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s \n", argv[0]);
+	return (0);
+}
