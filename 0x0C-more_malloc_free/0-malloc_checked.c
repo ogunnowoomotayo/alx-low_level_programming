18aaa168f7fa8c682196946864115d8724971654
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-10-07 18:26:45 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
