2a575f8edf3ddafa790418f9ab2aa2323e29b71d
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-10-09 13:29:10 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int j, i;
+	int num_1, num_2;
+	int sum;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (98);
+			}
+		}
+	}
+	num_1 = atoi(argv[1]);
+	num_2 = atoi(argv[2]);
+	sum = num_1 * num_2;
+	printf("%d\n", sum);
+	return (0);
+}
