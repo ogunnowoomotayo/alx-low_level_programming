358f5856a86827d73101c03c94fbdf5541685783
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-10-10 03:09:24 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct to define a new structure dog
+ * @name: 1st element
+ * @age: 2nd element
+ * @owner: 3rd element
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
