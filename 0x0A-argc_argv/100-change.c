787a307285d74e101094e5534a42d2f3721b6393
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-10-14 03:14:19 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int num, g, ttol;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	ttol = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (g = 0; g < 5 && num >= 0; g++)
+	{
+		while (num >= coins[g])
+		{
+			ttol++;
+			num -= coins[g];
+		}
+	}
+
+	printf("%d\n", ttol);
+	return (0);
+}
+
