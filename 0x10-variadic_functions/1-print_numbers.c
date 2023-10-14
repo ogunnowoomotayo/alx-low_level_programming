df5299fc5af8d6b1c07dc937a25837228e5ecb8b
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2023-10-14 00:08:00 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,12 +17,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(nums, n);
 	for (i = 0; i < n; i++)
 	{
+		printf("\n");
 		printf("%d", va_arg(nums, int));
 		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
-			printf("\n");
 			va_end(nums);
 	}
 }
